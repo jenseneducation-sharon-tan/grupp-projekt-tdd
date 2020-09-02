d768b3ef24cacb444f4a2d731679076d426630ec
madokafujimakiahlsen <madoka.fujimakiahlsen@gmail.com>
2020-09-02 22:34:27 +0200

    change text on test for ThankYou

--- tests/unit/ThankYou.spec.js
+++ tests/unit/ThankYou.spec.js
@@ -4,8 +4,8 @@ import {
   createLocalVue,
   RouterLinkStub,
 } from "@vue/test-utils";
+
 import ThankYou from "@/views/ThankYou.vue";
-import Home from "@/views/Home.vue";
 import VueRouter from "vue-router";
 
 const localVue = createLocalVue();
@@ -26,7 +26,7 @@ describe("ThankYou", () => {
     });
   });
 
-  it("should redirect to '/' when clicking on 'Till butiken' button", async () => {
+  it("should renders a router-link tag with to /", async () => {
     //Arrange
     const expectedUrl = "/";
     const button = wrapper.findComponent(RouterLinkStub);
@@ -35,14 +35,14 @@ describe("ThankYou", () => {
     expect(button.props("to")).toBe(expectedUrl);
   });
 
-  it("should be 'http://localhost:8080/' on url after clicking on 'Till butiken' button", async () => {
-    // console.log(wrapper.vm.$route); // innehåller information om routes
-    // //Arrange
-    // const expectedUrl = "http://localhost:8080/";
-    // const button = wrapper.find(RouterLinkStub);
-    // // Act
-    // await button.trigger("click");
-    // //Assert
-    // expect(button.props("to")).toBe(expectedUrl);
-  });
+  //it("should redirect to '/' when clicking on 'Till butiken' button", async () => {
+  // console.log(wrapper.vm.$route); // innehåller information om routes
+  // //Arrange
+  // const expectedUrl = "http://localhost:8080/";
+  // const button = wrapper.find(RouterLinkStub);
+  // // Act
+  // await button.trigger("click");
+  // //Assert
+  // expect(button.props("to")).toBe(expectedUrl);
+  //});
 });
