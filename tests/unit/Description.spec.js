a503d6f3ee09c72638e4dedd5616a0654daff645
Cindy Peng <cindy002587@gmail.com>
2020-09-02 14:21:13 +0200

    test case

---
+++ tests/unit/Description.spec.js
@@ -0,0 +1,10 @@
+/*import { mount } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
+import Description from '@/views/Description.vue'
+
+describe('Description',()=>{
+    let wrapper
+    it('should show details about product when click on product in home page', async()=>{
+
+    })
+})*/
