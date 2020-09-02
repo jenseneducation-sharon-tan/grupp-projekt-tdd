444cac1173798c1c1cf9ac549ed1eb92fe1616a4
richa310878 <54705026+richa310878@users.noreply.github.com>
2020-09-02 02:38:26 +0200

    cartlist done

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,41 +1,66 @@
 <template>
-  <div>
-      <section class="cartWrapper">
-      <img id="cartIcon" src="../assets/cart.svg" alt="Bag/Cart" />
-      <section class="lengthWrapper">
-        <!--span>{{cart.length}}</span-->
-      </section>
-      </section>
+  <div class="bag">
+    <router-link to="/ShoppingBag">
+      <div class="counter">
+        <p class="counter__num">{{ counter }}</p>
+      </div>
+      <div class="cartWrapper">
+        <img class="cartIcon" src="@/assets/shopping-cart.svg" alt="Bag/Cart" />
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+	computed: {
+    counter() {
+      let total = 0;
+      this.$root.$data.products.map(product => (total += product.count));
+      return total;
+    }
+  }
+};
 </script>
 
 <style lang ="scss" scoped>
-.cartWrapper {
-  z-index: 999;
-  display: flex;
-  background: rgb(83, 204, 83);
-  border-radius: 100%;
-  margin: 1rem;
-  height: 2rem;
-  width: 2rem;
-  padding: 1rem;
-  margin-bottom: 5rem;
+.bag {
   position: absolute;
-  right: 0;
+  z-index: 999;
+  top: 20px;
+  right: 20px;
+}
+.cartWrapper {
+  background-color: rgb(83, 204, 83);
+  border-radius: 50%;
+  height: 3rem;
+  width: 3rem;
   cursor: pointer;
-  img {
-    width: 100%;
+
+  .cartIcon {
+    width: 20px;
+    height: 20px;
+    margin: 20px auto auto 5px;
   }
-  &:hover{
-      -webkit-animation:wiggle 1.5s linear infinite;
-  animation:wiggle 1.5s linear infinite;
+  &:hover {
+    -webkit-animation: wiggle 1.5s linear infinite;
+    animation: wiggle 1.5s linear infinite;
   }
 }
+.counter {
+  width: 20px;
+  height: 20px;
+  background-color: #ff9be9;
+  border-radius: 50%;
+  position: absolute;
+  margin-top: 2px;
+  margin-left: 15px;
 
+  &__num {
+    color: white;
+    margin-top: 1px;
+    text-align: center;
+    font-size: 12px;
+  }
+}
 </style>
