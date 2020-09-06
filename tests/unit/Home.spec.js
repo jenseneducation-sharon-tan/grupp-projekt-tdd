432cfa3d7c2a63ec16d94488e2f7dcc2632f3a20
Cindy Peng <cindy002587@gmail.com>
2020-09-06 23:10:41 +0200

    update Home.spec.js

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -12,77 +12,45 @@ describe("Home", () => {
         image: "",
         price: 23,
         unit: "kg",
-        count: 0,
-      },
-    ],
+        count: 0
+      }
+    ]
   };
-  it("should renders data from main.js", /*async*/ () => {
+
+  it("should hava $root receive data from main.js", () => {
     const vm = new Vue({
-      el: "#app",
-      data: data,
+      data: data
     });
     console.log(data);
-    /* vm.$data === data; */
-    const wrapper = mount(Home, { vm });
-    console.log(wrapper);
-    // await window.trigger("keyup");
-    let findProduct = wrapper.findComponent(ProductList);
-    console.log(findProduct);
-    let findFruit = findProduct.findAll("#allFruits").wrappers;
-    console.log(findFruit);
-    let elementCount = findFruit.length;
-    expect(elementCount).toBe(data.products.length);
-    /* const wrapper = mount(Home, { mocks: { $root } }); */
-    /* wrapper.vm.$root = { data: true }; 
-    const productListComponent = wrapper.findComponent(ProductList);
-    //list of images
-    const findProductComponent = productListComponent.findAllComponents(Product)
-      .wrappers;
-    console.log(findProductComponent);
-    const hasImgId = findProductComponent[0].attributes("id");
-    console.log(hasImgId);
-    /*  await hasImgId.trigger("click"); 
-    expect(hasImgId).toBe("1");
-  /*it("should show id '1' when click on 'banan'", async () => {
-    const wrapper = mount(Home, { products });
-    const productListComponent = wrapper.findComponent(ProductList);
-    //list of images
-    const findProductComponent = productListComponent.findAllComponents(Product)
-      .wrappers;
-    console.log(findProductComponent);
-    const hasImgId = findProductComponent[0].attributes("id");
-    console.log(hasImgId);
-    /*  await hasImgId.trigger("click"); 
-    expect(hasImgId).toBe("1");
-  }); 
-  /* it("should show details about product when click on product in home page", async () => {
-    const wrapper = mount(Home, {
-      stubs: {
-        RouterLink: RouterLinkStub,
-      },
-      mocks: {
-        $root: {
-          data: {
-            products: [
-              {
-                name: "Banan",
-                id: 1,
-                image: "",
-                price: 23,
-                unit: "kg",
-                count: 0,
-              },
-            ],
-          },
-        },
-      },
-    });
-    const img = wrapper.findAll(".fruit").wrappers;
-    console.log(img);
-    console.log(wrapper.html());
-    const link = img.find((i) => i[0]);
-    await link.trigger("click");
-    expect(wrapper.find(RouterLinkStub).props().to).toBe("/description/1");
-  }); */
+
+    let computedProducts = vm.products;
+    let expected = computedProducts;
+
+    expect(expected).toBe(data.products);
   });
+
+  it("should have #allFruits div in Home.vue", () => {
+    const vm = new Vue({
+      mount: "#app",
+      data: data
+    });
+    const wrapper = mount(Home, { vm, stubs: ["router-link", "router-view"] });
+    let computedProducts = vm.products;
+    console.log(computedProducts);
+
+    let finProductList = wrapper.findComponent(ProductList);
+    console.log(finProductList.props());
+    let findFruits = finProductList.findAll("#allFruits");
+    let elementCount = findFruits.length;
+    expect(elementCount).toBe(1);
+  });
+
+  /*it('should have .fruit div in #allFruits', ()=>{
+     //let getFruit = findFruits.findComponent(Product);
+    //console.log(getFruit);
+    //let fruitDiv = getFruit.findAll(".fruitName");
+    //let fruitDiv = getFruit.findAll(".fruit");
+  })*/
+
+  //expect(wrapper.find(RouterLinkStub).props().to).toBe("/description/1");
 });
